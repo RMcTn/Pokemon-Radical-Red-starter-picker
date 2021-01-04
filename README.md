490030a6cd0701d400220fae679a5910d43bc6eb
Reece McTiernan <rmctiernan@pm.me>
2021-01-04 23:55:34 +0000

    Read starters from file and change in ROM

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pokemon Radical Red starter picker
+
+Requires python3 
+
+## Instructions
+Program expects Radical Red ROM to be called Pokemon Radical Red.gba in the same directory as the script.
+
+
+Create a starters.txt file in the same directory as the script containing 1-3 lines of pokdex IDs
+
+Example:
+```
+5
+9
+44
+```
+
+Run with the following in the terminal 
+```
+python3 starters.py
+```
---
+++ starter.py
@@ -0,0 +1,29 @@
+import sys
+starter_pokemon_offsets = [0x169BB5, 0x169DB8, 0x169D82]
+
+starters = [1, 4, 7]
+
+try:
+    with open("./starters.txt") as starters_file:
+        lines = starters_file.readlines()
+        number_of_lines = len(lines)
+        if number_of_lines == 0 or number_of_lines > 3:
+            sys.exit("Expecting 1-3 lines with pokedex id in each line in starters.txt file.")
+        for i, line in enumerate(lines):
+            try:
+                pokedex_id = int(line)
+            except ValueError:
+                sys.exit(f'Non number found on line {i+1} - {line}')
+                
+            starters[i] = int(line)
+except FileNotFoundError:
+    print("starters.txt file not found. Expecting 1-3 lines with pokedex id in each line.")
+
+
+
+with open("./Pokemon Radical Red.gba", "rb+") as rom:
+    for i, starter_offset in enumerate(starter_pokemon_offsets):
+        starter = starters[i].to_bytes(2, byteorder='little')
+        rom.seek(starter_offset)
+        rom.write(starter)
+    print(f'Changed starters to {starters}')
